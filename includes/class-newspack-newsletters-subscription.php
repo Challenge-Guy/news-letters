Challenge-Guy/news-letters

commit 861e5f8860ce5bd8dc79092535f859644bf76948

fix: adding contact to multiple groups within a list (#1460)

includes/class-newspack-newsletters-subscription.php
@@ -614,29 +614,30 @@ class Newspack_Newsletters_Subscription {
 	 *
 	 * @return array|WP_Error Contact data if it was added, or error otherwise.
 	 */
-	private static function add_contact_to_provider( $contact, $lists, $is_updating = false ) {
+	private static function add_contact_to_provider( $contact, $lists = [], $is_updating = false ) {
 		$provider = Newspack_Newsletters::get_service_provider();
 		$errors   = new WP_Error();
 		$result   = [];
 
-		if ( empty( $lists ) ) {
-			try {
-				$result = $provider->add_contact( $contact );
-			} catch ( \Exception $e ) {
-				$errors->add( 'newspack_newsletters_subscription_add_contact', $e->getMessage() );
-			}
-		} else {
-			foreach ( $lists as $list_id ) {
-				try {
-					$result = $provider->add_contact_handling_local_list( $contact, $list_id );
-				} catch ( \Exception $e ) {
-					$errors->add( 'newspack_newsletters_subscription_add_contact', $e->getMessage() );
-				}
-			}
+		try {
+			$result = $provider->add_contact_with_groups( $contact, $lists );
+		} catch ( \Exception $e ) {
+			$errors->add( 'newspack_newsletters_subscription_add_contact', $e->getMessage() );
 		}
+
 		if ( is_wp_error( $result ) ) {
 			$errors->add( $result->get_error_code(), $result->get_error_message() );
 		}
+
+		// Handle local lists feature.
+		foreach ( $lists as $list_id ) {
+			try {
+				$provider->add_contact_handling_local_list( $contact, $list_id );
+			} catch ( \Exception $e ) {
+				$errors->add( 'newspack_newsletters_subscription_handling_local_list', $e->getMessage() );
+			}
+		}
+
 		$result = $errors->has_errors() ? $errors : $result;
 
 		/**

includes/service-providers/class-newspack-newsletters-service-provider.php
@@ -438,11 +438,8 @@ abstract class Newspack_Newsletters_Service_Provider implements Newspack_Newslet
 	}
 
 	/**
-	 * Add or update contact to a list, but handling local Subscription Lists
-	 *
-	 * The difference between this method and add_contact is that this method will identify and handle local lists
-	 *
-	 * If the $list_id informed is a local list, it will read its settings and call add_contact with the list associated and also add the tag to the contact
+	 * Handle adding to local lists.
+	 * If the $list_id is a local list, a tag will be added to the contact.
 	 *
 	 * @param array  $contact      {
 	 *    Contact data.
@@ -453,35 +450,24 @@ abstract class Newspack_Newsletters_Service_Provider implements Newspack_Newslet
 	 * }
 	 * @param string $list_id      List to add the contact to.
 	 *
-	 * @return array|WP_Error Contact data if it was added, or error otherwise.
+	 * @return true|WP_Error True or error.
 	 */
 	public function add_contact_handling_local_list( $contact, $list_id ) {
+		if ( ! static::$support_local_lists ) {
+			return true;
+		}
 		if ( Subscription_List::is_local_form_id( $list_id ) ) {
 			try {
 				$list = Subscription_List::from_form_id( $list_id );
-
 				if ( ! $list->is_configured_for_provider( $this->service ) ) {
 					return new WP_Error( 'List not properly configured for the provider' );
 				}
 				$list_settings = $list->get_provider_settings( $this->service );
-
-				$added_contact = $this->add_contact( $contact, $list_settings['list'] );
-
-				if ( is_wp_error( $added_contact ) ) {
-					return $added_contact;
-				}
-
-				if ( static::$support_local_lists ) {
-					$this->add_esp_local_list_to_contact( $contact['email'], $list_settings['tag_id'], $list_settings['list'] );
-				}
-
-				return $added_contact;
-
+				return $this->add_esp_local_list_to_contact( $contact['email'], $list_settings['tag_id'], $list_settings['list'] );
 			} catch ( \InvalidArgumentException $e ) {
 				return new WP_Error( 'List not found' );
 			}
 		}
-		return $this->add_contact( $contact, $list_id );
 	}
 
 	/**

includes/service-providers/mailchimp/class-newspack-newsletters-mailchimp.php
@@ -8,6 +8,7 @@
 defined( 'ABSPATH' ) || exit;
 
 use DrewM\MailChimp\MailChimp;
+use Newspack\Newsletters\Subscription_List;
 use Newspack\Newsletters\Subscription_Lists;
 
 /**
@@ -1048,6 +1049,48 @@ final class Newspack_Newsletters_Mailchimp extends \Newspack_Newsletters_Service
 	}
 
 	/**
+	 * Add contact to a list with multiple groups.
+	 *
+	 * @param array $contact The contact, as for the add_contact method.
+	 * @param array $lists List IDs to add the contact to.
+	 */
+	public function add_contact_with_groups( $contact, $lists ) {
+		$results = [];
+		$by_list = [];
+		foreach ( $lists as $list_id ) {
+			$list = $this->maybe_extract_group_list( $list_id );
+			if ( ! $list ) {
+				// It might be a local list – the list id has to be extracted from the DB.
+				$list = Subscription_List::from_form_id( $list_id );
+				if ( ! $list->is_configured_for_provider( $this->service ) ) {
+					return new WP_Error( 'List not properly configured for the provider' );
+				}
+				$list_settings = $list->get_provider_settings( $this->service );
+				if ( $list_settings !== null ) {
+					// Empty groups – just add to the list.
+					$group_ids = [];
+					$by_list[ $list_settings['list'] ] = $group_ids;
+				}
+				continue;
+			}
+			$list_id  = $list['list_id'];
+			$group_id = $list['group_id'];
+			if ( ! isset( $contact['interests'] ) ) {
+				$contact['interests'] = [];
+			}
+			if ( isset( $by_list[ $list_id ] ) ) {
+				$by_list[ $list_id ][] = $group_id;
+			} else {
+				$by_list[ $list_id ] = [ $group_id ];
+			}
+		}
+		foreach ( $by_list as $list_id => $group_ids ) {
+			$results[] = $this->add_contact( $contact, $list_id, $group_ids );
+		}
+		return $results;
+	}
+
+	/**
 	 * Add contact to a list.
 	 *
 	 * @param array  $contact      {
@@ -1058,10 +1101,11 @@ final class Newspack_Newsletters_Mailchimp extends \Newspack_Newsletters_Service
 	 *    @type string[] $metadata Contact additional metadata. Optional.
 	 * }
 	 * @param string $list_id      List to add the contact to.
+	 * @param string $group_ids    Group IDs within the list to add the contact to.
 	 *
 	 * @return array|WP_Error Contact data if it was added, or error otherwise.
 	 */
-	public function add_contact( $contact, $list_id = false ) {
+	public function add_contact( $contact, $list_id = false, $group_ids = [] ) {
 		if ( false === $list_id ) {
 			return new WP_Error( 'newspack_newsletters_mailchimp_list_id', __( 'Missing list id.' ) );
 		}
@@ -1158,6 +1202,12 @@ final class Newspack_Newsletters_Mailchimp extends \Newspack_Newsletters_Service
 				$update_payload['interests'] = [
 					$group_id => true,
 				];
+			}
+			if ( ! empty( $group_ids ) ) {
+				$update_payload['interests'] = [];
+				foreach ( $group_ids as $group_id ) {
+					$update_payload['interests'][ $group_id ] = true;
+				}
 			}
 
 			// If we're subscribing the contact to a newsletter, they should have some status

commit 276618a2c1aa9214a66779412b821ddc661558a3

fix: multiple lists handling
Fixes a regression introduced in #1460

includes/class-newspack-newsletters-subscription.php
@@ -620,7 +620,15 @@ class Newspack_Newsletters_Subscription {
 		$result   = [];
 
 		try {
-			$result = $provider->add_contact_with_groups( $contact, $lists );
+			if ( method_exists( $provider, 'add_contact_with_groups' ) ) {
+				$result = $provider->add_contact_with_groups( $contact, $lists );
+			} elseif ( empty( $lists ) ) {
+				$result = $provider->add_contact( $contact );
+			} else {
+				foreach ( $lists as $list_id ) {
+					$result = $provider->add_contact( $contact, $list_id );
+				}
+			}
 		} catch ( \Exception $e ) {
 			$errors->add( 'newspack_newsletters_subscription_add_contact', $e->getMessage() );
 		}